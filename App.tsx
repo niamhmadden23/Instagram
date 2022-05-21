4656f3474b42fb10ddad91e41d17607f2054f32c
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-05-21 14:45:55 +0100

    style: added home vector icon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {View, Text} from 'react-native';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import Feather from 'react-native-vector-icons/Feather';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           fontSize: fonts.size.xxlg,
         }}>
         Hello World
+        <Feather name="home" size={25} />
       </Text>
     </View>
   );
