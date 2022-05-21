0ced480e81ae066ddb6e9bac3c8a97f07dec5b9a
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-05-21 14:33:11 +0100

    styling: global fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import {View, Text} from 'react-native';
 import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: colors.primary}}>Hello World</Text>
+      <Text
+        style={{
+          color: colors.primary,
+          fontWeight: fonts.weight.normal,
+          fontSize: fonts.size.xxlg,
+        }}>
+        Hello World
+      </Text>
     </View>
   );
 };
